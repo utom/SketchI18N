1efff9eafffd735d83db99ca2ce404bc8b10e1b5
utom <utombox@gmail.com>
2016-09-22 01:07:22 +0800

    Fixed 10.12 bugs

--- SketchI18N.sketchplugin/Contents/Sketch/app.js
+++ SketchI18N.sketchplugin/Contents/Sketch/app.js
@@ -4,12 +4,14 @@ var onRun = function(context) {
         command = context.command,
         identifier = command.identifier(),
         resourcesPath = command.pluginBundle().url().path(),
+        macOSVersion =  Number(NSDictionary.dictionaryWithContentsOfFile("/System/Library/CoreServices/SystemVersion.plist").objectForKey("ProductVersion")),
         language = NSUserDefaults.standardUserDefaults().objectForKey("AppleLanguages").objectAtIndex(0),
+        language = (macOSVersion >= 10.12)? language.split("-").slice(0, -1).join("-"): language,
         languagePath = resourcesPath + "/Contents/Resources/i18n/" + language + ".json";
 
     if (NSFileManager.defaultManager().fileExistsAtPath(languagePath)) {
         var mocha = Mocha.sharedRuntime();
-        i18n = [NSString stringWithContentsOfFile: languagePath encoding: NSUTF8StringEncoding error: nil];
+        i18n = NSString.stringWithContentsOfFile_encoding_error(languagePath, NSUTF8StringEncoding, nil);
 
         if (!NSClassFromString("SketchI18NPluginManager")) {
             mocha.loadFrameworkWithName_inDirectory("SketchI18NPlugin", resourcesPath + "/Contents/Resources");
